90362e8b2222e8fa33385373c5d1902f4bdf4f45
mimenza <mimenzae@gmail.com>
2024-02-29 10:08:04 +0100

    working version of the analizer v1

--- srcs/08_steps.c
+++ srcs/08_steps.c
@@ -6,7 +6,7 @@
 /*   By: emimenza <emimenza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 09:30:01 by emimenza          #+#    #+#             */
-/*   Updated: 2024/02/28 14:55:45 by emimenza         ###   ########.fr       */
+/*   Updated: 2024/02/29 10:06:39 by emimenza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,8 @@ t_step	*init_first_step(t_input **struct_input, t_token *input_token)
 	return (first_step);
 }
 
+//LA FUNCION REDUCE DEPENDE DE QUE CASOS HACE SEGFAULT
+
 int	start_anaylizer(t_input **struct_input, t_token *input_token)
 {
 	t_step		*step;
@@ -95,18 +97,6 @@ int	start_anaylizer(t_input **struct_input, t_token *input_token)
 		print_token_list(c_step->tree_stack);
 		printf("\n");
 	
-		if (step->input == NULL && step->tree_stack != NULL && stack_size(step->tree_stack) == 1)
-		{
-			t_token *stack_top = last_node_stack(step->tree_stack);
-			if (stack_top->type == 100)
-			{
-				// Regresa hasta el paso 0
-				while (c_step->step_nbr != 0)
-				{
-					ret_to_prev(&c_step);
-				}
-			}
-		}
 		//conseguimos la opcion default siempre que la tengamos y la opcion disponible depende de nuestro c_token
 		def_option = find_option(c_step->state, -1);
 		available_option = find_option(c_step->state, c_token->type);
@@ -119,6 +109,11 @@ int	start_anaylizer(t_input **struct_input, t_token *input_token)
 			printf("MISMO QUE EL DEFAULT\n");
 			apply_action(def_option, &c_step, c_token, &end_flag);
 		}
+		//No tenemos defualt y tenemos avail option
+		else if ((available_option == NULL) && (def_option == NULL) && (c_token->type >= 100) && (c_step->state_nbr != 0))
+		{
+			ret_to_prev(&c_step);
+		}
 		//miramos si es mismo tipo que un token simple (si es asi aplicamos la accion y movemos al siguiente paso creandolo)
 		else if ((available_option != NULL))
 		{
@@ -127,7 +122,7 @@ int	start_anaylizer(t_input **struct_input, t_token *input_token)
 			add_step(*struct_input, available_option, c_step->tree_stack, c_step->input, &c_step);
 			
 			//(si vamos a un estado de una opcion que es token simple nuestro token a evaluar es el ultimo de stack, sino el primero del input)
-			if (c_step->input && (c_token->type >= 100 || c_token->type == 5))
+			if (c_step->input && (c_token->type >= 100 || c_token->type == 1 || c_token->type == 2 || c_token->type == 3 || c_token->type == 4 || c_token->type == 5))
 				c_token = c_step->input;
 		}
 		else if ((available_option == NULL) && def_option)
@@ -140,29 +135,10 @@ int	start_anaylizer(t_input **struct_input, t_token *input_token)
 		}
 		else if ((available_option == NULL) && (def_option == NULL))
 		{
-			//si no coincide con ninguno y no tenemos default, systax error;
-			if ((c_step->input->type -2 ) && (stack_size(c_step->tree_stack) == 0) && (last_node_stack(c_step->tree_stack)->type == 100))
-			{
-				printf("\n\033[0;35mRETURNING TO 0\n\033[0m\n");
-				while (c_step->step_nbr != 0)
-					ret_to_prev(&c_step);
-			}
-			else
-				break;
+			printf("SYNTAX ERROR\n");
+			break;
 		}
 		end++;
 	}
-
-	// print_steps_info(step);
-	// printf("-----INPUT:-----\n");
-	// print_token_list(step->input);
-	// printf("-----STACK:-----\n");
-	// print_token_list(step->tree_stack);
-	
-	// if ((step->tree_stack != NULL) && ((stack_size(step->tree_stack) == 1) && (step->tree_stack->type == 100)))
-	// {
-	// 	return (TRUE);
-	// }
-	// else
-	// 	return (FALSE);
 }
+
